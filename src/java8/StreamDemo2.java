f82fea1b799411ce39b603f258a112ef5f11ee29
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-11-07 10:46:13 +0100

    stream cd

---
+++ src/java8/StreamDemo2.java
@@ -0,0 +1,42 @@
+package java8;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class StreamDemo2 {
+    public static void main(String[] args) {
+        List<String> names = new ArrayList<>();
+        names.add("Dawid");
+        names.add("Dagmara");
+        names.add("Filip");
+        names.add("Wojtas");
+        names.add("Anna");
+
+
+
+
+        long count = names.stream()
+                .sorted()
+                .distinct()
+                .count();
+
+
+        System.out.println(count);
+
+        names.stream()
+                .sorted((name1, name2) -> name1.length()-name2.length())
+                .forEach( (String  name) -> System.out.println(name));
+
+
+
+        List<Character> firstLetters =  names.stream()
+                .map(name -> name.charAt(0))   // wymieniam imie na 1 litere imienia
+                .filter(firstLetter -> firstLetter != 'A') // odsiewam litery A
+                .collect(Collectors.toList());
+
+        System.out.println(firstLetters);
+
+
+    }
+}

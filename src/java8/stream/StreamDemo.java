62936506b7051b62e114b3d52859a12e1ef28a05
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-11-07 10:15:54 +0100

    wykorzystanie zmiennych w lambdach

--- src/java8/stream/StreamDemo.java
+++ src/java8/stream/StreamDemo.java
@@ -13,11 +13,36 @@ public class StreamDemo {
         names.add("Filip");
         names.add("Wojtas");
 
+       int  count = 0;
+        final Counter counterObject = new Counter();
         for (String name : names) {
             System.out.println(name);
+            if (name.startsWith("D")) {
+                count++;
+            }
         }
+
+        names.forEach(name -> System.out.println(name.toUpperCase()+ "!!!") );
+        names.forEach(name -> {
+            if (name.startsWith("D")) {
+              //  count++; // w lambdach i klasach wewnetrznych nie można używać zmiennych lokalnych (z wyjątkiem stałych)
+                counterObject.increment();// sposób obejścia
+            }
+        } );
 
         names.forEach(name -> System.out.println(name));
     }
 
+}
+
+class Counter{
+   private int count;
+
+    public void increment() {
+        count++;
+    }
+
+    public int getCount() {
+        return count;
+    }
 }

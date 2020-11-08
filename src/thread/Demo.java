f3a066880fab6aa45fc06dac38b878577d86be1f
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-11-08 10:51:16 +0100

    threads

--- src/thread/Demo.java
+++ src/thread/Demo.java
@@ -2,9 +2,12 @@ package thread;
 
 public class Demo {
     public static void main(String[] args) {
-        Runnable zadanie =   () -> {
-            for (int i = 0; i < 1_000; i++) {
-                System.out.println(i + " Pozdrowienia z osobnego wątku 1");
+        Runnable zadanie =  new Runnable() {
+            @Override
+            public void run() {
+                for (int i = 0; i < 1_000; i++) {
+                    System.out.println(i + " Pozdrowienia z osobnego wątku 1");
+                }
             }
         };
 

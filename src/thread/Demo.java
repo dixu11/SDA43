d25f1317130ddcc2f02a2acb3b84a0f464f8945d
Daniel Szlicht <daniel.szlicht25@gmail.com>
2020-11-08 10:50:42 +0100

    threads

---
+++ src/thread/Demo.java
@@ -0,0 +1,27 @@
+package thread;
+
+public class Demo {
+    public static void main(String[] args) {
+        Runnable zadanie =   () -> {
+            for (int i = 0; i < 1_000; i++) {
+                System.out.println(i + " Pozdrowienia z osobnego wątku 1");
+            }
+        };
+
+        Runnable zadanie2 =   () -> {
+            for (int i = 0; i < 1_000; i++) {
+                System.out.println(i + " Pozdrowienia z osobnego wątku 2");
+            }
+        };
+
+        Thread pracownik = new Thread(zadanie);
+        Thread pracownik2 = new Thread(zadanie2);
+
+        pracownik.start();
+        pracownik2.start();
+
+
+
+
+    }
+}
